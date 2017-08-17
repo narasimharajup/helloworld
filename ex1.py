5afa0b009d2de397ad508184402379e17b9269c9
narasimharajup <narasimharaju.perumal@gmail.com>
2017-08-17 16:39:01 +0530

    main added, deleted ex1

--- ex1.py
+++
@@ -1,5 +0,0 @@
-print "Hello World"
-print 'Nice things'
-print "Wow! 'good'"
-print 'I "said" do not take this.'
-print "B4N"
---
+++ main.py
@@ -0,0 +1,23 @@
+print ("Hello World")
+print ('Nice things')
+print ("Wow! 'good'")
+
+
+"""
+print 'I "said" do not take this.'
+print #"B4N"
+print "Is it greater", 5 > 2
+cars = 30
+passengers = 300
+print "no of cars %d " % cars
+print "how many passenger can fit in a car : ", passengers  / cars
+x = "there are %d types of people" % 10
+binary = "binary"
+do_not = "don't"    
+y = "Those who know %s and those who %s." %(binary, do_not)
+print x
+print y
+
+formatter = "%r %r %r %r"
+print formatter % (1, 2, 3, 4)
+print formatter % ("one","two","three","four") """
